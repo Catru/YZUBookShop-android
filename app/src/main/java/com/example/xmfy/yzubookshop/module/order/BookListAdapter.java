2d3461520facbdf5fa025ac79220085b07c246ea
xmfy <a411919924@icloud.com>
2018-03-08 21:53:22 +0800

    确认订单界面完成，部分

---
+++ app/src/main/java/com/example/xmfy/yzubookshop/module/order/BookListAdapter.java
@@ -0,0 +1,10 @@
+package com.example.xmfy.yzubookshop.module.order;
+
+import android.widget.BaseAdapter;
+
+/**
+ * Created by xmfy on 2018/3/8.
+ */
+
+public class BookListAdapter extends BaseAdapter {
+}
---
+++ app/src/main/java/com/example/xmfy/yzubookshop/module/order/OrderConfirmActivity.java
@@ -0,0 +1,13 @@
+package com.example.xmfy.yzubookshop.module.order;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OrderConfirmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_confirm);
+    }
+}
