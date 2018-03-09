c775e2435177f6d35017a5d2273a635f0119aa2e
xmfy <a411919924@icloud.com>
2018-03-09 21:58:01 +0800

    完成订单确认界面

---
+++ app/src/main/java/com/example/xmfy/yzubookshop/module/order/DeliverySelectAdapter.java
@@ -0,0 +1,10 @@
+package com.example.xmfy.yzubookshop.module.order;
+
+import android.widget.BaseAdapter;
+
+/**
+ * Created by xmfy on 2018/3/9.
+ */
+
+public class DeliverySelectAdapter extends BaseAdapter {
+}
